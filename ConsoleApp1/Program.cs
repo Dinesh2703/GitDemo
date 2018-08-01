a74d5d88db2beee1c95a657df9c9f04aaa3918b9
Dinesh <dinesh.srmu12@gmail.com>
2018-08-01 12:34:18 +0530

    pgm changed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -23,11 +23,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int x = 30;
-            int y = 20;
+            int y = 50;
             op m = new op();
 
             Console.WriteLine("sum is" + m.sum(x, y));
-            Console.WriteLine("Multiplication is" + m.mul(x, y));
+            Console.WriteLine("Value is" + m.mul(x, y));
             Console.ReadKey();
         }
     }
