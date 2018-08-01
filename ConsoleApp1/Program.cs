dd81269aeca31eefe95fa9e7036555aee7bd19d8
Dinesh <dinesh.srmu12@gmail.com>
2018-08-01 12:07:39 +0530

    master commited

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class op
+    {
+         public int sum(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            op m = new op();
+
+            Console.WriteLine("sum is" + m.sum(x, y));
+            Console.ReadKey();
+        }
+    }
+}
